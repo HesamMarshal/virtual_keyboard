13b9d50c96c13580bbf8633debdb792a3dda03cb
Hesam Marshal <heak82@hotmail.com>
2022-01-01 19:27:58 +0330

    replace inline codes with Consts

--- main.py
+++ main.py
@@ -2,38 +2,40 @@
 # cvzone version 1.4.1 TODO: Upgrade to latest version
 # pynput v.1.7.3
 
-
-
-
-
 import cv2  # version 4.5.3
 from cvzone.HandTrackingModule import HandDetector
 from time import sleep
 import numpy as np
-
 import cvzone
-from pynput.keyboard import Controller            # pynput v.1.7.3
-
+from pynput.keyboard import Controller
 
 print('cv2.version:',cv2.__version__)
-frame_width = 1280
-frame_height = 720
 
+
+
+#CONSTS
 DEFAULT_WEBCAM = 0
 XPLIT_WEBCAM = 1
 IRIUN_WEBCAM = 2
 
+frame_width = 1280
+frame_height = 720
 
-# TODO: Make COLOR Constants,
-#  ALl Colors upercase ,
-yellow = (0,255,255)
+# COLORS
+YELLOW = (0,255,255)
 DARK_YELLOW = (0,175,175)
-purple = (255,0,255)
-white = (255,255,255)
+PURPLE = (255,0,255)
+WHITE = (255,255,255)
 GREEN = (0,255,0)
-black = (0,0,0)
-# TODO: Create more of them
-key_color = yellow
+BLACK = (0,0,0)
+
+# CONFIG
+FONT_COLOR = BLACK
+FONT_FAMILY = cv2.FONT_HERSHEY_PLAIN
+KEY_COLOR = YELLOW
+HOVER = DARK_YELLOW
+KEY_DIVIDER = GREEN
+CLICK_SIZE = 20 # TODO : use it
 
 cap = cv2.VideoCapture(IRIUN_WEBCAM)
 #cap.set(3, frame_width)    # width
@@ -54,8 +56,8 @@ def drawAll(img, buttonList):
         w, h = button.size
         cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
                                    20, rt=0)
-        cv2.rectangle(img, button.pos, (x + w, y + h), key_color, cv2.FILLED)
-        cv2.putText(img, button.text, (x + 18, y + 70), cv2.FONT_HERSHEY_PLAIN, 4, black, 4)
+        cv2.rectangle(img, button.pos, (x + w, y + h), KEY_COLOR, cv2.FILLED)
+        cv2.putText(img, button.text, (x + 18, y + 70), cv2.FONT_HERSHEY_PLAIN, 4, FONT_COLOR, 4)
     return img
 
 
@@ -92,6 +94,7 @@ class Button():
 
     #    return img
 
+# Draw numbers on the image
 buttonList = []
 base_loc = 300
 for x in range(0, 3):
@@ -112,28 +115,26 @@ while True:
             x,y = button.pos
             w,h = button.size
 
-            # TODO: Replace DARK_YELLOW with hover and set the hover to it.
+
             # TODO : Add select font at up.
             if x < lmlist[8][0] <x+w and y < lmlist[8][1]<y+h:
-                cv2.rectangle(img, button.pos, (x + w, y + h), DARK_YELLOW, cv2.FILLED)
-                cv2.putText(img, button.text, (x + 18, y + 70), cv2.FONT_HERSHEY_PLAIN, 4, black, 4)
+                cv2.rectangle(img, button.pos, (x + w, y + h), HOVER, cv2.FILLED)
+                cv2.putText(img, button.text, (x + 18, y + 70), FONT_FAMILY, 4, FONT_COLOR, 4)
                 l,_,_ = detecter.findDistance(8,12,img,draw=False)
 
-
+                # TODO: Make CLICK as Const and define it up the file
                 # clicked
                 if l < 20:
                     keyboard.press(button.text)
                     print(l)
-                    cv2.rectangle(img, button.pos, (x + w, y + h),GREEN , cv2.FILLED)
-                    cv2.putText(img, button.text, (x + 18, y + 70), cv2.FONT_HERSHEY_PLAIN, 4, black, 4)
+                    # TODO: Change Green to a meaning full Const
+                    cv2.rectangle(img, button.pos, (x + w, y + h),KEY_DIVIDER , cv2.FILLED)
+                    cv2.putText(img, button.text, (x + 18, y + 70), FONT_FAMILY, 4, FONT_COLOR, 4)
                     finalText += button.text
                     sleep(0.5)
 
-    cv2.rectangle(img, (50,350), (600,450), DARK_YELLOW, cv2.FILLED)
-    cv2.putText(img, finalText, (60, 425), cv2.FONT_HERSHEY_PLAIN, 4, black, 4)
-
-
-
+    cv2.rectangle(img, (50,350), (600,450), HOVER, cv2.FILLED)
+    cv2.putText(img, finalText, (60, 425), FONT_FAMILY, 4, FONT_COLOR, 4)
 
 
     #cv2.resize(img, dim, interpolation=cv2.INTER_AREA)  # (img, frameWidth, frameHeight)
